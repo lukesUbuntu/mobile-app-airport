bbbd5ee4635d0e4225a88b16355470fe5f2d2257
Michael murray <michael.murray@hotmail.co.nz>
2015-07-21 23:22:55 +1200

    removed bad code from camera....
    *removed bad code from camera controller
    *added link to log in page from index

--- app.js
+++ app.js
@@ -22,24 +22,7 @@ angular.module('app', ['ionic'])
 })
 
 
-    .controller("cameracontrol", function ($scope) {
-
-        $scope.camera.getPicture(onSuccess, onFail, {
-            quality: 50,
-            destinationType: Camera.DestinationType.FILE_URI
-        });
-
-        function onSuccess(imageURI) {
-            var image = document.getElementById('myImage');
-            image.src = imageURI;
-        }
-
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
-
-
-    })
+    
 
    .controller('taxilist', function ($scope) {
        
