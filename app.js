1455976550544c5a6cf70d2adb8240e250e9f393
Luke Hardiman <luke@hardiman.co.nz>
2015-07-26 20:25:55 +1200

    taxi_info test
    Returns test call back from api

--- app.js
+++ app.js
@@ -66,14 +66,20 @@ angular.module('app', ['ionic'])
 })
 
 //search a taxi record
-.controller('searchTaxi',['$scope', '$http', function ($scope, $http){
+.controller('searchTaxi', ['$scope', '$http',  function ($scope, $http) {
   
     $scope.search = function (registration) {
+        
         console.log("reg", registration)
         //check reg
         //@todo add no registration was provided
         if (typeof registration == "undefined" || registration.length < 3) return false; //return error message no reg
 
+        $scope.show = function () {
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+        };
         //https://docs.angularjs.org/api/ng/service/$http
         $http.jsonp(api.call('getRegistration'),{
             params : {
@@ -84,14 +90,24 @@ angular.module('app', ['ionic'])
             //check response
             if (response.success) {
                 console.log("response.data", response.data)
+                $scope.taxi_info = response.data;
+                $scope.searchResults = true;
             }
             else
-            console.log("bad response", response);
-          
+                console.log("bad response", response);
+
+            $scope.hide = function () {
+                $ionicLoading.hide();
+            };
         }).error(function (response, status, headers, config) {
             console.log("error data", response);
+            $scope.hide = function () {
+                $ionicLoading.hide();
+            };
         })
     }
+
+   
         
 
 }])
